7905b154f5f326d0c30e010289a757c3688c75a9
zhaijialong <jialongzhai@gmail.com>
2021-09-02 17:59:25 +0800

    hlslpp

---
+++ external/hlslpp/include/swizzle/hlsl++_matrix_row3_1.h
@@ -0,0 +1,17 @@
+// -------------------------------
+// _mNM
+// -------------------------------
+
+hlslpp_swizzle_start swizzle1<0> _m30; hlslpp_swizzle_end
+hlslpp_swizzle_start swizzle2<0, 0> _m30_m30; hlslpp_swizzle_end
+hlslpp_swizzle_start swizzle3<0, 0, 0> _m30_m30_m30; hlslpp_swizzle_end
+hlslpp_swizzle_start swizzle4<0, 0, 0, 0> _m30_m30_m30_m30; hlslpp_swizzle_end
+
+// -------------------------------
+// _NM
+// -------------------------------
+
+hlslpp_swizzle_start swizzle1<0> _41; hlslpp_swizzle_end
+hlslpp_swizzle_start swizzle2<0, 0> _41_41; hlslpp_swizzle_end
+hlslpp_swizzle_start swizzle3<0, 0, 0> _41_41_41; hlslpp_swizzle_end
+hlslpp_swizzle_start swizzle4<0, 0, 0, 0> _41_41_41_41; hlslpp_swizzle_end
