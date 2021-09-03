b1df3b7636eea34336b62bf8a4fba1cd313ded65
zhaijialong <jialongzhai@gmail.com>
2021-09-03 18:21:03 +0800

    templates

--- source/utils/math.h
+++ source/utils/math.h
@@ -8,25 +8,15 @@ using namespace linalg::aliases;
 
 using uint = uint32_t;
 
-inline float degree_to_randian(float degree)
+template<class T>
+inline T degree_to_randian(T degree)
 {
 	const float PI = 3.141592653f;
 	return degree * PI / 180.0f;
 }
 
-inline float3 degree_to_randian(float3 degree)
-{
-	const float PI = 3.141592653f;
-	return degree * PI / 180.0f;
-}
-
-inline float radian_to_degree(float radian)
-{
-	const float PI = 3.141592653f;
-	return radian * 180.0f / PI;
-}
-
-inline float3 radian_to_degree(float3 radian)
+template<class T>
+inline T radian_to_degree(T radian)
 {
 	const float PI = 3.141592653f;
 	return radian * 180.0f / PI;
