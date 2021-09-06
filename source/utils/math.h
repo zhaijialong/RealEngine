5de099d797e8564f6247afa11f87dd4dadfa86a8
zhaijialong <jialongzhai@gmail.com>
2021-09-06 10:57:03 +0800

    fix quaternion

--- source/utils/math.h
+++ source/utils/math.h
@@ -27,29 +27,28 @@ inline float3 vector_to_float3(const std::vector<float>& v)
 	return float3(v[0], v[1], v[2]);
 }
 
-template<class T> 
-linalg::vec<T, 4> rotation_quat(const vec<T, 3>& euler_angles) //roll-pitch-yaw order
+inline float4 rotation_quat(const float3& euler_angles) //pitch-yaw-roll order
 {
-	float cr = std::cos(euler_angles[0] * 0.5f);
-	float sr = std::sin(euler_angles[0] * 0.5f);
-	float cp = std::cos(euler_angles[1] * 0.5f);
-	float sp = std::sin(euler_angles[1] * 0.5f);
-	float cy = std::cos(euler_angles[2] * 0.5f);
-	float sy = std::sin(euler_angles[2] * 0.5f);
+	float cx = std::cos(euler_angles.x * 0.5f);
+	float sx = std::sin(euler_angles.x * 0.5f);
+	float cy = std::cos(euler_angles.y * 0.5f);
+	float sy = std::sin(euler_angles.y * 0.5f);
+	float cz = std::cos(euler_angles.z * 0.5f);
+	float sz = std::sin(euler_angles.z * 0.5f);
 
-	const vec<T, 4> q
+	const float4 q
 	{
-		cy * cp * sr - sy * sp * cr,
-		sy * cp * sr + cy * sp * cr,
-		sy * cp * cr - cy * sp * sr,
-		cy * cp * cr + sy * sp * sr
+		cx * sy * sz + sx * cy * cz,
+		cx * sy * cz - sx * cy * sz,
+		cx * cy * sz - sx * sy * cz,
+		cx * cy * cz + sx * sy * sz
 	};
 
 	return q;
 }
 
-//https://docs.microsoft.com/en-us/windows/win32/direct3d10/d3d10-d3dxmatrixorthooffcenterlh
 inline float4x4 ortho_matrix(float l, float r, float b, float t, float n, float f)
 {
+	//https://docs.microsoft.com/en-us/windows/win32/direct3d10/d3d10-d3dxmatrixorthooffcenterlh
 	return { {2 / (r - l), 0, 0, 0}, {0, 2 / (t - b), 0, 0}, {0, 0, 1 / (f - n), 0}, {(l + r) / (l - r), (t + b) / (b - t), n / (n - f), 1} };
 }
