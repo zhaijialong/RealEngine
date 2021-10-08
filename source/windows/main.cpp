6e35d0460609c30d016719f2de5885a49e9e7ac7
zhaijialong <jialongzhai@gmail.com>
2021-10-08 11:39:15 +0800

    move to windows folder

--- source/windows/main.cpp
+++ source/windows/main.cpp
@@ -1,6 +1,6 @@
 #include "core/engine.h"
 #include "imgui/imgui_impl_win32.h"
-#include "../resource.h"
+#include "resource.h"
 #include <Windows.h>
 
 extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
