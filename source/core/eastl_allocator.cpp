89c38b7be0158b6b791abbfc33b7acd189866dae
zhaijialong <jialongzhai@gmail.com>
2022-03-04 13:34:31 +0800

    eastl 3.18.00

---
+++ source/core/eastl_allocator.cpp
@@ -0,0 +1,11 @@
+#include "eastl_allocator.h"
+
+void* operator new[](size_t size, const char* pName, int flags, unsigned debugFlags, const char* file, int line)
+{
+    return malloc(size);
+}
+
+void* operator new[](size_t size, size_t alignment, size_t alignmentOffset, const char* pName, int flags, unsigned debugFlags, const char* file, int line)
+{
+    return _aligned_malloc(size, alignment);
+}
---
+++ source/core/eastl_allocator.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <new>
+
+void* operator new[](size_t size, const char* pName, int flags, unsigned debugFlags, const char* file, int line);
+void* operator new[](size_t size, size_t alignment, size_t alignmentOffset, const char* pName, int flags, unsigned debugFlags, const char* file, int line);
