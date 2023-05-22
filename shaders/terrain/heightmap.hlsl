1eed2da023624084b8a1c11360435974d575108e
Jialong Zhai <jialongzhai@gmail.com>
2023-05-22 19:05:07 +0800

    Update heightmap.hlsl

--- shaders/terrain/heightmap.hlsl
+++ shaders/terrain/heightmap.hlsl
@@ -9,7 +9,7 @@ cbuffer CB : register(b0)
 [numthreads(8, 8, 1)]
 void main(uint3 dispatchThreadID : SV_DispatchThreadID)
 {    
-    RWTexture2D<float> outputTexture = ResourceDescriptorHeap[c_outputTexture];
+    RWTexture2D<float4> outputTexture = ResourceDescriptorHeap[c_outputTexture];
     
     float width, height;
     outputTexture.GetDimensions(width, height);
@@ -19,9 +19,12 @@ void main(uint3 dispatchThreadID : SV_DispatchThreadID)
     uv *= 2.0;
     uv += float2(fmod(c_seed, 43.5), fmod(c_seed, 63.7));
     
-    float f = uber_noise(uv, 12, -1.0, 0.6, 1.0, 0.5, 0.5, 1.8, 0.6).x;
-    f = f * 0.5 + 0.1;
-    f = pow(f, 1.5);
+    float rock = uber_noise(uv, 12, -1.0, 0.6, 1.0, 0.5, 0.5, 1.8, 0.6).x;
+    rock = rock * 0.5 + 0.1;
+    rock = pow(rock, 1.5);
     
-    outputTexture[dispatchThreadID.xy] = f;
+    float sand = fbm(uv * 2.0);
+    sand = max(0, sand * 0.1);
+    
+    outputTexture[dispatchThreadID.xy] = float4(rock, sand, 0, 0);
 }
